47bf05b842cc4edf98a3a74da229ed5fc07002e3
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-07-01 09:56:18 -0500

    Commit inicial

---
+++ src/main/java/com/eci/ARSW/TalleresDEP/TallerDEP/TallerDepApplication.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.TalleresDEP.TallerDEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerDepApplication.class, args);
+	}
+
+}
